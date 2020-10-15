59f9c5814eaf5eac52c4e8d0ddae5c98d8a58423
Firecul <Firecul666@gmail.com>
2020-10-15 18:19:54 +0100

    Change to just using one handling file
    Ease of use basically

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # GTA-V-Default-Handling-Files---FiveM-resource
+
 A copy of the default handling.meta files found in GTA V. Saved for future reference and set up as a FiveM resource.
 
-To use drop the 'handling' folder into your resources folder and add `start handling` to your server.cfg file.
+To use drop the 'handling' folder into your resources folder and add `start handling` to your server.cfg file.  
 
-The Unified-handling.meta file is there to combine all wheeled vehicles together into one file for my own use, this is in alphabetical order and with common formatting as R* should have done in the first place.
+The Unified-handling.meta file is there to combine all wheeled vehicles together into one file, this is in alphabetical order and with common formatting as R_ should have done in the first place.  
+The rest of the files are ripped straight from R_'s files for a reference.
--- handling/__resource.lua
+++ handling/__resource.lua
@@ -1,77 +1,7 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 files {
-	'data/common-handling.meta',
-	'data/mpapartment-handling.meta',
-	'data/mpassault-handling.meta',
-	'data/mpbattle-handling.meta',
-	'data/mpbeach-handling.meta',
-	'data/mpbiker-handling.meta',
-	'data/mpbusiness-handling.meta',
-	'data/mpbusiness2-handling.meta',
-	'data/mpchristmas2-handling.meta',
-	'data/mpchristmas2017-handling.meta',
-	'data/mpchristmas2018-handling.meta',
-	'data/mpexecutive-handling.meta',
-	'data/mpgunrunning-handling.meta',
-	'data/mphalloween-handling.meta',
-	'data/mpheist-handling.meta',
-	'data/mpheist3-handling.meta',
-	'data/mphipster-handling.meta',
-	'data/mpimportexport-handling.meta',
-	'data/mpindependence-handling.meta',
-	'data/mpjanuary2016-handling.meta',
-	'data/mplowrider-handling.meta',
-	'data/mplowrider2-handling.meta',
-	'data/mplts-handling.meta',
-	'data/mpluxe-handling.meta',
-	'data/mpluxe2-handling.meta',
-	'data/mppilot-handling.meta',
-	'data/mpsmuggler-handling.meta',
-	'data/mpspecialraces-handling.meta',
-	'data/mpstunt-handling.meta',
-	'data/mpsum-handling.meta',
-	'data/mpvalentines-handling.meta',
-	'data/mpvalentines2-handling.meta',
-	'data/mpvinewood-handling.meta',
-	'data/mpxmas_604490-handling.meta',
-	'data/spupgrade-handling.meta'
-	'data/update-handling.meta',
+	'Unified-handling.meta',
 }
 
-data_file 'HANDLING_FILE' 'data/common-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpapartment-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpassault-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpbattle-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpbeach-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpbiker-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpbusiness-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpbusiness2-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpchristmas2-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpchristmas2017-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpchristmas2018-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpexecutive-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpgunrunning-handling.meta'
-data_file 'HANDLING_FILE' 'data/mphalloween-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpheist-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpheist3-handling.meta'
-data_file 'HANDLING_FILE' 'data/mphipster-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpimportexport-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpindependence-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpjanuary2016-handling.meta'
-data_file 'HANDLING_FILE' 'data/mplowrider-handling.meta'
-data_file 'HANDLING_FILE' 'data/mplowrider2-handling.meta'
-data_file 'HANDLING_FILE' 'data/mplts-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpluxe-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpluxe2-handling.meta'
-data_file 'HANDLING_FILE' 'data/mppilot-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpsmuggler-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpspecialraces-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpstunt-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpsum-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpvalentines-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpvalentines2-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpvinewood-handling.meta'
-data_file 'HANDLING_FILE' 'data/mpxmas_604490-handling.meta'
-data_file 'HANDLING_FILE' 'data/spupgrade-handling.meta'
-data_file 'HANDLING_FILE' 'data/update-handling.meta'
+data_file 'HANDLING_FILE' 'Unified-handling.meta'
